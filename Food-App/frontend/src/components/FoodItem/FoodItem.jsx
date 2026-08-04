bad993114c4200211144c8f6e933d46befff7b1d
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2026-08-04 22:31:18 +0530

    creating food app using react

--- Food-App/frontend/src/components/FoodItem/FoodItem.jsx
+++ Food-App/frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/asset'
 const FoodItem = ({id,name,price,desc,image}) => {
+
+  const [itemCount,setItemCount]=useState(0) 
   return (
     <div>
       <div className="food-item">
         <div className="food-item-img-container">
-            <img src={image} className='food-item-image' alt="" />
+            <img src={image}  className='food-item-image' alt="" />
+            {itemCount? <img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} />  : <div className='food-item-counter'>
+              <img onClick={()=>setItemCount(prev=>prev-1)}src={assets.remove_icon_red} alt="" />
+              <p>{itemCount}</p>
+              <img onClick={()=>setItemCount(prev=>prev+1)}src={assets.add_icon_green} alt="" />
+            </div> }
         </div>
         <div className="food-item-info">
             <div className="food-item-name-rating">
